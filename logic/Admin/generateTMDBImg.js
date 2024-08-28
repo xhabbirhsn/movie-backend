0a8b569207aeca9311bcea451f2d135c02d16d14
xhabbirhn <shabbirjpl38@gmail.com>
2024-08-29 01:32:07 +0530

    add movie credit

--- logic/Admin/generateTMDBImg.js
+++ logic/Admin/generateTMDBImg.js
@@ -1,10 +1,24 @@
-const tmdbImg = require("../../utils/tmdbImg");
+const { tmdbImg, movieCredit } = require("../../utils/tmdbImg");
 
 async function genrateTMDBimgLink(imgId) {
     return new Promise(async (resolve, reject) => {
         try {
             const imageId = await tmdbImg(imgId);
+            const credit = await movieCredit(imgId);
             const imageUrl = `https://image.tmdb.org/t/p/w500${imageId.poster_path}`;
+
+            const categorizedActors = {
+                actor: [],
+                director: [],
+            };
+            for (let i = 0; i < 5; i++) {
+                if (credit.cast[i].known_for_department == "Acting") {
+                    categorizedActors.actor.push(credit.cast[i].original_name);
+                }
+                if (credit.crew[i].known_for_department == "Directing") {
+                    categorizedActors.director.push(credit.crew[i].original_name);
+                }
+            }
             const data = {
                 poster: imageUrl,
                 imageId: imageId.id,
@@ -15,8 +29,9 @@ async function genrateTMDBimgLink(imgId) {
                 overview: imageId.overview,
                 release_date: imageId.release_date,
                 runtime: imageId.runtime,
+                hero: categorizedActors
             };
-            resolve(imageId);
+            resolve(data);
         } catch (error) {
             throw new Error("Error while generating img: " + error.message);
         }
--- utils/tmdbImg.js
+++ utils/tmdbImg.js
@@ -21,4 +21,24 @@ async function tmdbImg(imgId) {
     })
 }
 
-module.exports = tmdbImg
+async function movieCredit(imgId){
+    return new Promise(async (resolve, reject) => {
+        try{
+            const options = {
+                method: 'GET',
+                url: `https://api.themoviedb.org/3/movie/${imgId}/credits?language=en-US`,
+                headers: {
+                  accept: 'application/json',
+                  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTExMWU1YjBiNDYzY2Q2OWY0OTYxZWMxYTEzOWI3YyIsIm5iZiI6MTcyNDYwODk1My40NTcwMjIsInN1YiI6IjY2YjRhNmYwYWY5YWVlMDE4MjI0MTdkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.d5O9NYr0UrPXh7tqy4j8bXvOsWAKqzlfhgzG95CHv2w'
+                }
+              };
+
+              const response = await axios.request(options);
+              resolve(response.data)
+        }catch(error) {
+            reject(new Error('Error getting movie credit: ' + error.message));
+        }
+    })
+}
+
+module.exports = {tmdbImg, movieCredit}
