2b473ab7253944cf845c30d83455be1a4055d809
xhabbirhn <shabbirjpl38@gmail.com>
2024-09-03 09:45:10 +0530

    change in addMovie

--- controllers/Admin/addMovie.js
+++ controllers/Admin/addMovie.js
@@ -1,26 +1,61 @@
-const appRoot = require('app-root-path');
-const path = require('path');
-const { failureResponse, successResponse } = require(path.join(appRoot.path, '/utils/responseSchema'));
-const statusCodes = require(path.join(appRoot.path, '/utils/stausCodes.json'));
-const addMovies = require('../../logic/Admin/addMovie');
+const appRoot = require("app-root-path");
+const path = require("path");
+const { failureResponse, successResponse } = require(path.join(
+    appRoot.path,
+    "/utils/responseSchema"
+));
+const statusCodes = require(path.join(appRoot.path, "/utils/stausCodes.json"));
+const addMovies = require("../../logic/Admin/addMovie");
 
 const addMovie = async (req, res) => {
     try {
-        const movieName = req.body.name;
-        const banner = req.body.banner;
-        let result = await addMovies(movieName, banner).catch(error => {
-            let failure = failureResponse(statusCodes.BAD_REQUEST.status, error, statusCodes.BAD_REQUEST.statusCode)
-            res.status(failure.statusCode).send(failure.body)
-        })
-        if (result) {
-            let success = successResponse(result, statusCodes.OK.statusCode)
-            res.status(success.statusCode).send(success.body)
-        }
+        const details = {
+            imgId: req.body.imgId,
+            generateLink: req.body.generateLink,
+            movieHeading: req.body.movieHeading,
+            selectedDate: req.body.selectedDate,
+            description: req.body.description,
+            aboutSite: req.body.aboutSite,
+            title: req.body.title,
+            genres: req.body.genres,
+            releaseDate: req.body.releaseDate,
+            rating: req.body.rating,
+            overview: req.body.overview,
+            director: req.body.director,
+            actor: req.body.actor,
+            movieAndQualityDes: req.body.movieAndQualityDes,
+            movieName: req.body.movieName,
+            duration: req.body.duration,
+            language: req.body.language,
+            subtitles: req.body.subtitles,
+            size: req.body.size,
+            quality: req.body.quality,
+            format: req.body.format,
+            urls: req.body.urls,
+            entries: req.body.entries
+          };
+          
 
+        let result = await addMovies(details).catch((error) => {
+            let failure = failureResponse(
+                statusCodes.BAD_REQUEST.status,
+                error,
+                statusCodes.BAD_REQUEST.statusCode
+            );
+            res.status(failure.statusCode).send(failure.body);
+        });
+        if (result) {
+            let success = successResponse(result, statusCodes.OK.statusCode);
+            res.status(success.statusCode).send(success.body);
+        }
     } catch (error) {
-        console.log(error)
-        let failure = failureResponse(statusCodes.INTERNAL_SERVER_ERROR.status, error.message, statusCodes.INTERNAL_SERVER_ERROR.statusCode)
-        res.status(failure.statusCode).send(failure.body)
+        console.log(error);
+        let failure = failureResponse(
+            statusCodes.INTERNAL_SERVER_ERROR.status,
+            error.message,
+            statusCodes.INTERNAL_SERVER_ERROR.statusCode
+        );
+        res.status(failure.statusCode).send(failure.body);
     }
-}
-module.exports = addMovie
+};
+module.exports = addMovie;
--- logic/Admin/addMovie.js
+++ logic/Admin/addMovie.js
@@ -1,15 +1,11 @@
 const Movie = require('../../helpers/mongoSchema/addMovie')
 
-async function addMovies(movieName, bannerPath) {
+async function addMovies(details) {
     return new Promise(async (resolve, reject) => {
-        const newMovie = new Movie({
-            movieName: movieName,
-            banner: bannerPath
-        });
-    
         try {
-            const savedMovie = await newMovie.save();
-            resolve({ message: 'Movie added successfully', movie: savedMovie });
+            console.log("detailsss", details)
+            // const savedMovie = await newMovie.save();
+            // resolve({ message: 'Movie added successfully', movie: savedMovie });
         } catch (error) {
             throw new Error('Error saving movie: ' + error.message);
         }
